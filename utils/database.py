45746e2316fb1e4b9b19e3a5e9fdfaf1c3cd253a
Swayam <108215141+Swayam25@users.noreply.github.com>
2022-10-08 16:20:40 +0000

    Changed

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,6 @@
 import os, sys
 from deta import Deta
 
-deta = Deta(os.environ["SECRET_KEY"])
+deta = Deta(os.environ["DETA_PROJECT_KEY"])
 user = deta.Base("confessioner_users")
 posts = deta.Base("confessioner_posts")
